d0666e1a4f1da2b7a2f0d90f92017c0e7c4b4256
salgado2004 <leosalgado2004@gmail.com>
2024-11-10 11:50:46 -0300

    #19 refactor: rename package dao -> connection

---
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/connection/json/UserJsonDao.java
@@ -0,0 +1,2 @@
+package br.ufpr.webII.trabalhoFinal.infra.connection.json;public class UserJsonDao {
+}
---
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/connection/sql/UserSQLDao.java
@@ -0,0 +1,2 @@
+package br.ufpr.webII.trabalhoFinal.infra.connection.sql;public class UserSQLDao {
+}
