c08b1b22a9e1c7ed2ac775ee5302adf113478cae
AlissonGSantos <alisson.gabriel@ufpr.br>
2024-11-10 22:38:47 -0300

    #25 feat: Update DAO factory use.

--- src/main/java/br/ufpr/webII/trabalhoFinal/infra/service/EquipmentCategoryService.java
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/service/EquipmentCategoryService.java
@@ -1,6 +1,8 @@
 package br.ufpr.webII.trabalhoFinal.infra.service;
 
 import java.util.List;
+
+import br.ufpr.webII.trabalhoFinal.infra.connection.DaoFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,34 +10,42 @@ import br.ufpr.webII.trabalhoFinal.domain.equipment.EquipmentCategoryInputDTO;
 import br.ufpr.webII.trabalhoFinal.domain.equipment.EquipmentCategoryUpdateDTO;
 import br.ufpr.webII.trabalhoFinal.domain.equipment.EquipmentCategory;
 import br.ufpr.webII.trabalhoFinal.infra.exceptions.RequestException;
-import br.ufpr.webII.trabalhoFinal.infra.repository.EquipmentDao;
+import br.ufpr.webII.trabalhoFinal.infra.connection.EquipmentDao;
 
 @Service
 public class EquipmentCategoryService {
     @Autowired
-    private EquipmentDao equipmentDAO;
-
+    private DaoFactory daoFactory;
     public void createEquipmentCategory(EquipmentCategoryInputDTO data) {
         try {
             EquipmentCategory equipmentCategory = new EquipmentCategory(data);
+            EquipmentDao equipmentDAO = daoFactory.getEquipmentDao();
             equipmentDAO.insert(equipmentCategory);
         } catch (IllegalArgumentException e){
+            throw new RequestException(e.getMessage());
+        } catch (Exception e){
             throw new RequestException(e.getMessage());
         }
     }
 
     public List<EquipmentCategory> listEquipmentCategories() {
         try {
-            return equipmentDAO.selectAll();    
-        } catch (IllegalArgumentException e){
+            EquipmentDao equipmentDAO = daoFactory.getEquipmentDao();
+            return equipmentDAO.listAll();
+        } catch (IllegalArgumentException e) {
+            throw new RequestException(e.getMessage());
+        } catch (Exception e) {
             throw new RequestException(e.getMessage());
         }
     }
 
     public void deleteEquipmentCategory(Long id) {
         try {
-            equipmentDAO.delete(id);
+            EquipmentDao equipmentDAO = daoFactory.getEquipmentDao();
+            equipmentDAO.delete(new EquipmentCategory(id));
         } catch (IllegalArgumentException e){
+            throw new RequestException(e.getMessage());
+        } catch (Exception e){
             throw new RequestException(e.getMessage());
         }
     }
@@ -43,8 +53,11 @@ public class EquipmentCategoryService {
     public void updateEquipmentCategory(EquipmentCategoryUpdateDTO data) {
         try {
             EquipmentCategory equipmentCategory = new EquipmentCategory(data);
+            EquipmentDao equipmentDAO = daoFactory.getEquipmentDao();
             equipmentDAO.update(equipmentCategory);
         } catch (IllegalArgumentException e){
+            throw new RequestException(e.getMessage());
+        } catch (Exception e){
             throw new RequestException(e.getMessage());
         }
     }
