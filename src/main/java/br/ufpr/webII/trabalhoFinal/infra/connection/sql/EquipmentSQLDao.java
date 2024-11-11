04831bb8f0874a08c37ba3f3f810eaef866b6c31
AlissonGSantos <alisson.gabriel@ufpr.br>
2024-11-10 21:23:12 -0300

    #25 feat: make EquipmentSQLDao queries

--- src/main/java/br/ufpr/webII/trabalhoFinal/infra/connection/sql/EquipmentSQLDao.java
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/connection/sql/EquipmentSQLDao.java
@@ -8,6 +8,9 @@ import br.ufpr.webII.trabalhoFinal.domain.equipment.EquipmentCategory;
 import br.ufpr.webII.trabalhoFinal.infra.connection.ConnectionFactory;
 import br.ufpr.webII.trabalhoFinal.infra.connection.EquipmentDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,27 +32,70 @@ public class EquipmentSQLDao implements EquipmentDao {
 
     @Override
     public void insert(EquipmentCategory element) throws Exception {
-
+        try(Connection con = connectionFactory.getConnection();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO public.equipment_category (name) VALUES (?)")){
+            ps.setString(1, element.getCategoryDesc());
+            ps.executeUpdate();
+        } catch (Exception e){
+            throw new Exception("Erro ao inserir categoria de equipamento", e);
+        }
     }
 
     @Override
     public void update(EquipmentCategory objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try(Connection con = connectionFactory.getConnection();
+            PreparedStatement ps = con.prepareStatement("UPDATE public.equipment_category SET name = ? WHERE id = ?")){
+            ps.setString(1, objeto.getCategoryDesc());
+            ps.setLong(2, objeto.getEquipCategoryId());
+            ps.executeUpdate();
+        } catch (Exception e){
+            throw new Exception("Erro ao atualizar categoria de equipamento", e);
+        }
     }
 
     @Override
     public void delete(EquipmentCategory objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try(Connection con = connectionFactory.getConnection();
+            PreparedStatement ps = con.prepareStatement("UPDATE public.equipment_category SET active = false WHERE id = ?")){
+            ps.setLong(1, objeto.getEquipCategoryId());
+            ps.executeUpdate();
+        } catch (Exception e){
+            throw new Exception("Erro ao deletar categoria de equipamento", e);
+        }
     }
 
     @Override
     public List<EquipmentCategory> listAll() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ArrayList<EquipmentCategory> categories = new ArrayList<>();
+        try(Connection con = connectionFactory.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM public.equipment_category WHERE active = true")){
+            while (ps.getResultSet().next()){
+                EquipmentCategory category = new EquipmentCategory();
+                category.setEquipCategoryId(ps.getResultSet().getLong("id"));
+                category.setCategoryDesc(ps.getResultSet().getString("name"));
+                categories.add(category);
+            }
+            return categories;
+        } catch (Exception e){
+            throw new Exception("Erro ao listar categorias de equipamento", e);
+        }
     }
 
     @Override
     public EquipmentCategory select(EquipmentCategory objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try(Connection con = connectionFactory.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM public.equipment_category WHERE id = ?")){
+            ps.setLong(1, objeto.getEquipCategoryId());
+            if (ps.getResultSet().next()){
+                EquipmentCategory category = new EquipmentCategory();
+                category.setEquipCategoryId(ps.getResultSet().getLong("id"));
+                category.setCategoryDesc(ps.getResultSet().getString("name"));
+                return category;
+            }
+            return null;
+        } catch (Exception e){
+            throw new Exception("Erro ao selecionar categoria de equipamento", e);
+        }
     }
     
 }
