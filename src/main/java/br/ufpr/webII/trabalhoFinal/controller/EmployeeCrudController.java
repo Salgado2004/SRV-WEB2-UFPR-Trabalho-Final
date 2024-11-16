0bd8c83904eca55693a360fc81bd93e85263c482
salgado2004 <leosalgado2004@gmail.com>
2024-11-16 15:56:46 -0300

    #24 wip: refactor employee controller

--- src/main/java/br/ufpr/webII/trabalhoFinal/controller/EmployeeCrudController.java
+++ src/main/java/br/ufpr/webII/trabalhoFinal/controller/EmployeeCrudController.java
@@ -2,17 +2,12 @@ package br.ufpr.webII.trabalhoFinal.controller;
 
 import br.ufpr.webII.trabalhoFinal.domain.user.employee.Employee;
 import br.ufpr.webII.trabalhoFinal.infra.connection.json.EmployeeJsonDao;
+import br.ufpr.webII.trabalhoFinal.infra.service.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/service/v1/employee")
@@ -20,6 +15,9 @@ public class EmployeeCrudController {
     
     @Autowired
     private EmployeeJsonDao employeeCrud;
+
+    @Autowired
+    private TokenService tokenService;
     
     @PostMapping()
     public ResponseEntity<String> newEmployee(@RequestBody @Valid Employee data) throws Exception {
@@ -28,21 +26,22 @@ public class EmployeeCrudController {
         return ResponseEntity.status(HttpStatus.CREATED).body("Empregado criada com sucesso!");
     }
   
-    @GetMapping("/detail/:id")
-    public ResponseEntity<String> detailEmployee() throws Exception{
+    @GetMapping("{id}")
+    public ResponseEntity<String> detailEmployee(@PathVariable Long id) throws Exception{
         employeeCrud.listAll();
         
         return ResponseEntity.ok("Detalhes da requisição de Empregados");
     }
 
-    @DeleteMapping("/delete/:id")
-    public ResponseEntity<String> deleteEmployee(Employee emp) throws Exception{
-        employeeCrud.delete(emp);
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id, @RequestHeader("Authorization") String auth) throws Exception{
+        Long idLogged = tokenService.getUserId(auth);
+        //employeeCrud.delete(emp);
         return ResponseEntity.status(HttpStatus.CREATED).body("Empregado deletado com sucesso");
     }
     
-    @PutMapping("/update/:id")
-    public ResponseEntity<String> updateEmployee(Employee emp) throws Exception{
+    @PutMapping()
+    public ResponseEntity<String> updateEmployee(@RequestBody @Valid Employee emp) throws Exception{
         employeeCrud.update(emp);
         return ResponseEntity.status(HttpStatus.CREATED).body("Empregado atualizado com sucesso");
     }
