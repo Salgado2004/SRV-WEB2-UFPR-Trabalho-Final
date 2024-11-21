85a3c8a88b8a9662e1facf55657a1336fdd6474d
salgado2004 <leosalgado2004@gmail.com>
2024-11-20 22:45:26 -0300

    fix: change frontend allowed port

--- src/main/java/br/ufpr/webII/trabalhoFinal/infra/config/WebConfig.java
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/config/WebConfig.java
@@ -10,32 +10,32 @@ import org.springframework.lang.NonNull;
 public class WebConfig implements WebMvcConfigurer {
     private static final String BASE_URL = "/service/v1";
     private static final String[] ALLOWED_ORIGINS = {
-        "http://localhost:3000"
+        "http://localhost:4200"
     };
     
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         // Auth Controller
-        addCorsMapping(registry, BASE_URL + "/auth/register", new String[]{"POST"});
-        addCorsMapping(registry, BASE_URL + "/auth/login", new String[]{"POST"});
+        addCorsMapping(registry, BASE_URL + "/auth/register", "POST");
+        addCorsMapping(registry, BASE_URL + "/auth/login", "POST");
         
         // Employee Controller
-        addCorsMapping(registry, BASE_URL + "/employee", new String[]{"POST", "GET"});
-        addCorsMapping(registry, BASE_URL + "/employee/{userId}", new String[]{"DELETE", "PUT"});
+        addCorsMapping(registry, BASE_URL + "/employee", "POST", "GET");
+        addCorsMapping(registry, BASE_URL + "/employee/{userId}", "DELETE", "PUT");
         
         // Equipment Category Controller        
-        addCorsMapping(registry, BASE_URL + "/equipment-category", new String[]{"POST", "GET", "PUT"});
-        addCorsMapping(registry, BASE_URL + "/equipment-category/{id}", new String[]{"DELETE"});
+        addCorsMapping(registry, BASE_URL + "/equipment-category", "POST", "GET", "PUT");
+        addCorsMapping(registry, BASE_URL + "/equipment-category/{id}", "DELETE");
 
         // Receipt Controller
-        addCorsMapping(registry, BASE_URL + "/receipt", new String[]{"GET"});
+        addCorsMapping(registry, BASE_URL + "/receipt", "GET");
 
         // Requests Controller        
-        addCorsMapping(registry, BASE_URL + "/requests", new String[]{"POST", "GET"});
-        addCorsMapping(registry, BASE_URL + "/requests/{id}", new String[]{"GET", "PUT"});
+        addCorsMapping(registry, BASE_URL + "/requests", "POST", "GET");
+        addCorsMapping(registry, BASE_URL + "/requests/{id}", "GET", "PUT");
     }
 
-    private void addCorsMapping(CorsRegistry registry, String path, String[] methods) {
+    private void addCorsMapping(CorsRegistry registry, String path, String... methods) {
         registry.addMapping(path)
                 .allowedOrigins(ALLOWED_ORIGINS)
                 .allowedMethods(methods);
