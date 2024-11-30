3c99197bb512f7ecc65f13d4eef285343a57bbb4
Matbaaz <mateus.bazan@gmail.com>
2024-11-30 00:09:57 -0300

    #37 wip: writing the context codes

--- src/main/java/br/ufpr/webII/trabalhoFinal/infra/service/PresentRequestToUserContext.java
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/service/PresentRequestToUserContext.java
@@ -4,10 +4,33 @@
  */
 package br.ufpr.webII.trabalhoFinal.infra.service;
 
+import br.ufpr.webII.trabalhoFinal.domain.request.Request;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author mateus
  */
 public class PresentRequestToUserContext {
+    
+    
+    private final String client = "SELECT * FROM public.request WHERE active = true AND customer_id = ?";
+    private final String employee = "SELECT * FROM public.request AS request INNER JOIN public.request_status AS status "
+            + "ON request.request_id = status.request_id"
+            + "WHERE status.in_charge_employee = ? OR status.status = OPEN";
+    
+    public PresentRequestToUserContext(){
+    }
+    
+    public List<Request> showRequestToUser(JWTToken token){
+        List<Request> listagem = new ArrayList<Request>();
+        try(//implementar conexão depois ou fazer a chamda do dau passando a string
+                ){
+            
+        }
+        
+        return listagem;
+    }
     
 }
