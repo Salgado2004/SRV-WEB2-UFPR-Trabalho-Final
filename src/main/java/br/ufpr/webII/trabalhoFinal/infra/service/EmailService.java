7e8ffc98f5cd22e46d66159fdd187f0e0f2f3af5
Pedro Souza <pedro.souza.1008@gmail.com>
2024-12-02 19:42:16 -0300

    #72 feat: template de email enviado

--- src/main/java/br/ufpr/webII/trabalhoFinal/infra/service/EmailService.java
+++ src/main/java/br/ufpr/webII/trabalhoFinal/infra/service/EmailService.java
@@ -2,10 +2,14 @@ package br.ufpr.webII.trabalhoFinal.infra.service;
 
 import br.ufpr.webII.trabalhoFinal.domain.email.MessageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
@@ -19,12 +23,19 @@ public class EmailService {
     @Autowired
     private JavaMailSender sender;
     
-    String emailBody(String senha){
-        return "Olá!\n\n"
-                + "Seja bem vindo ao melhor local para sua manutenção. Agradecemos o cadastro e estamos ansiosos para lhe atender.\n\n"
-                + "A sua senha se encontra abaixo (não a perca ou o acesso ao nosso serviço será totalmente perdido para esse email)\n\n"
-                + senha
-                + "\n\nAtenciosamente,\nTime ManuTADS";
+
+
+    public String emailBody(String senha) throws IOException {
+//  return "Olá!\n\n"
+//+ "Seja bem vindo ao melhor local para sua manutenção. Agradecemos o cadastro e estamos ansiosos para lhe atender.\n\n"
+//+ "A sua senha se encontra abaixo (não a perca ou o acesso ao nosso serviço será totalmente perdido para esse email)\n\n"
+//+ senha
+//+ "\n\nAtenciosamente,\nTime ManuTADS";
+
+        ClassPathResource resource = new ClassPathResource("templateEmail.html");
+        String htmlContent = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+
+        return htmlContent.replace("{{senha}}", senha);
     }
 
     public void sendEmail(MessageDTO messageDTO){
@@ -38,7 +49,7 @@ public class EmailService {
 
             sender.send(message);
 
-        } catch (MailException ex){
+        } catch (MailException | IOException ex){
             System.out.println(("Erro no envio do email!"+ex.getMessage()));
         }
 
